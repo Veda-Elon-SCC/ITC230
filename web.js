7f0e66389ac8d5587a1abd7980ae19d34c2e39ef
Veda Elon <veda.elon@seattlecentral.edu>
2018-04-15 20:50:43 -0700

    Add home.html index.js package.json

--- web.js
+++
@@ -1,6 +0,0 @@
-//Simple Page
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
