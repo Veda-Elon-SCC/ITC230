7c365ff370dd370bb9da022ca28fda615bf8604d
Veda Elon <veda.elon@seattlecentral.edu>
2018-04-18 17:42:20 -0700

    Code to load html. Yay! Thank you for the help!

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var http = require("http"); 
+var http = require("http");
 http.createServer(function(req,res) {
   var path = req.url.toLowerCase();
   switch(path) {
     case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page');
+      var fs = require("fs");
+      fs.readFile('home.html', function (err, data) {
+        if (err) return console.error(err);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data.toString());
+        console.log(data.toString());
+      });
+
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
