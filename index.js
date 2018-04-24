e0cb15ac865c96027d6dba043d66454a00e64c60
Veda Elon <veda.elon@seattlecentral.edu>
2018-04-23 17:47:37 -0700

    Add .gitignore

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ http.createServer(function(req,res) {
         res.end(data.toString());
         console.log(data.toString());
       });
-
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
