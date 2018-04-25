af64c1ccd11f872b1338f7aa5c71b6f357e10ae6
Veda Elon <veda.elon@seattlecentral.edu>
2018-04-25 14:05:01 -0700

    Add array.

---
+++ humans.js
@@ -0,0 +1,11 @@
+var humans = [
+  {firstName : "John", lastName : "Smith", born : 1970},
+  {firstName : "Mary", lastName : "Smith", born : 1972},
+  {firstName : "Johnathan", lastName : "Smith", born : 2000},
+  {firstName : "Maryanne", lastName : "Smith", born : 2002},
+  {firstName : "Pat", lastName : "Smith", born : 2004}
+];
+
+console.log(humans);
+console.log(humans[0]);
+console.log(humans[2]);
