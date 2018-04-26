a030a635ab71c8f836f32005198661eeb28215ed
Veda Elon <veda.elon@seattlecentral.edu>
2018-04-25 17:40:28 -0700

    Slightly Better

--- humans.js
+++ humans.js
@@ -1,13 +1,25 @@
 var humans = [
-  {firstName : "John", lastName : "Smith", born : 1970},
-  {firstName : "Mary", lastName : "Smith", born : 1972},
-  {firstName : "Johnathan", lastName : "Smith", born : 2000},
-  {firstName : "Maryanne", lastName : "Smith", born : 2002},
-  {firstName : "Pat", lastName : "Smith", born : 2004}
+  { firstName : "John", lastName : "Smith", born : 1970},
+  { firstName : "Mary", lastName : "Smith", born : 1972},
+  { firstName : "Johnathan", lastName : "Smith", born : 2000},
+  { firstName : "Maryanne", lastName : "Smith", born : 2002},
+  { firstName : "Pat", lastName : "Smith", born : 2004}
 ];
 
 function getAll() {
-       return humans.firstName;
+       return humans;
     };
 
-console.log(humans.getAll);
+console.log(getAll());
+
+function getOne() {
+       return humans[3];
+    };
+
+console.log(getOne());
+
+function get() {
+       return humans[3].firstName + ' ' + humans[3].lastName + ', born: ' + humans[3].born;
+    };
+
+console.log(get());
